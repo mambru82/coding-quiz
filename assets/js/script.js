a38476d90faa8681effd29c5d799cd4c7304da3b
mambru <ecastro82@gmail.com>
2020-12-27 15:45:49 -0600

    renders basic quiz structure, no functionality to buttons, no quiz logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,78 @@
+//Quiz questions and answers in an object oriented format
+var quizArray = [{
+  questionNumber: 1,
+  questionTxt: "A for loop is often used for the purposes of:",
+  answerTxt1: "Iterating through a string of values",
+  answerTxt2: "For forring your forest",
+  answerTxt3: "Flipping burgers",
+  answerTF1: true,
+  answerTF2: false,
+  answerTF3: false
+},
+{
+  questionNumber: 2,
+  questionTxt: "The main difference between a for loop and a while loop is:",
+  answerTxt1: "one fors, one whiles",
+  answerTxt2: "one sets an incremental loop, the other sets a loop with a fixed condition",
+  answerTxt3: "Your momma",
+  answerTF1: false,
+  answerTF2: true,
+  answerTF3: false 
+}]
+var questionsRemaining = quizArray.length;
+var quizScore = 0;
+var pageContentEl = document.querySelector("#page-content");
+//WHEN I click the start button
+  //Start of quiz function
+
+//THEN a timer starts and I am presented with a question
+
+//Transitioning from start to first question function
+
+//Quiz functionality
+  //Array of possible answers is presented as buttons 
+var createQuizQuestion = function(event){
+  var questionContainerEl = document.createElement("div");
+  questionContainerEl.className = "quiz-question";
+
+  // render quiz question
+  questionContainerEl.innerHTML = "<h3 class='question-number'>" +  quizArray[0].questionNumber + "</h3><span class='question-text'>" + quizArray[0].questionTxt + "</span>";
+
+  // render answer choices
+  var answerChoiceContainerEl = document.createElement("ol");
+  var answerChoiceEl = document.createElement("li");
+  var answerButtonEl = document.createElement("button");
+  answerButtonEl.textContent = quizArray[0].answerTxt1;
+  answerButtonEl.className = "btn answer-choice";
+
+  answerChoiceEl.appendChild(answerButtonEl);
+  answerChoiceContainerEl.appendChild(answerChoiceEl);
+  questionContainerEl.appendChild(answerChoiceContainerEl);
+  pageContentEl.appendChild(questionContainerEl);
+}
+
+var checkAnswerChoice = function(event) {
+  //compare the answer choice to answer key
+  //if answer key is true
+    //add answer to score
+  //if answer key is false
+    //subtract time
+    //move on to next question
+}
+
+
+  //When the correct button is pressed, the user is told it is correct and the quiz moves on
+    //Correct answer is added to correct tally
+
+  //when the incorrect button is pressed, the user is told it is incorrect and the quiz moves on
+    //the timer decreases
+
+//When either questions left or timer equals 0, quiz ends
+  //when timer ends, endOfQuiz function is initiated
+
+//User inputs their initials
+//Their score is saved to local storage 
+
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
@@ -39,4 +114,7 @@ function displayMessage() {
   }, 300);
 }
 
-startBtn.onclick = countdown;
+//startBtn.onclick = countdown;
+
+createQuizQuestion();
+
