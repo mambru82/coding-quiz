7a4fdac72e9f9c57e0dc7e0ce2591c79cd0dbb2f
mambru <ecastro82@gmail.com>
2020-12-27 21:37:25 -0600

    working quiz structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,16 @@ var quizArray = [{
   questionTxt: "The main difference between a for loop and a while loop is:",
   answerChoices: ["one fors, one whiles", "one sets an incremental loop, the other sets a loop with a fixed condition", "Your momma"],
   answerKey: [false, true, false]
+},
+{
+  questionNumber: 3,
+  questionTxt: "Happy now?",
+  answerChoices: ["no", "yes"],
+  answerKey: [false, true]
 }]
 var questionStatus=0;
 var quizScore = 0;
+var timeRemaining = 75;
 var pageContentEl = document.querySelector("#page-content");
 //WHEN I click the start button
   //Start of quiz function
@@ -21,8 +28,7 @@ var pageContentEl = document.querySelector("#page-content");
 var codingQuizStart = function(event) {
   var quizStartContainerEl = document.createElement("div");
   quizStartContainerEl.className = "starting-page";
-  quizStartContainerEl.innerHTML = "<h2 class='welcome-page'> Coding Quiz Challenge </h2>";
-  quizStartContainerEl.innerHTML = "<p class='welcome-content'> Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds! </p>";
+  quizStartContainerEl.innerHTML = "<h1 class='welcome-page'> Coding Quiz Challenge </h1> <p class='welcome-content'> Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds! </p>";
   var startButtonEl = document.createElement("button");
   startButtonEl.className = "btn start-button";
   startButtonEl.textContent = "Start Quiz";
@@ -31,13 +37,18 @@ var codingQuizStart = function(event) {
   pageContentEl.appendChild(quizStartContainerEl);
 }
 //THEN a timer starts and I am presented with a question
-
-//Transitioning from start to first question function
-
+var createFinalScore = function(event) {
+  var quizEndContainerEl = document.createElement ("div");
+  quizEndContainerEl.className = "end-screen";
+  quizEndContainerEl.innerHTML = "<h1 class='end-page'>All done! </h1> <p class='end-content'>Your final score is " + quizScore + ". </p>";
+  pageContentEl.appendChild(quizEndContainerEl);
+}
 //Quiz functionality
   //Array of possible answers is presented as buttons 
 var createQuizQuestion = function(questionStatus){
-  if (questionStatus < quizArray.length) {
+  if (questionStatus===0) {
+  var clearStart = document.querySelector(".starting-page"); 
+  clearStart.remove();}
   var questionContainerEl = document.createElement("div");
   questionContainerEl.className = "quiz-question";
   questionContainerEl.setAttribute("id", questionStatus);
@@ -59,49 +70,59 @@ var createQuizQuestion = function(questionStatus){
   }
   questionContainerEl.appendChild(answerChoiceContainerEl);
   pageContentEl.appendChild(questionContainerEl);}
-  else {console.log("end of the quiz!")}
-}
+  
+
 //task button handler will determine what happens with a click
+//CONSIDER using a switch for the taskButton a switch 
 var taskButtonHandler = function(event) {
   //get target element from event
   var targetEl = event.target;
   //if an answer choice was clicked
   if (targetEl.matches(".answer-choice")){
+    checkAnswerChoice(targetEl);
+  }
+  else if (targetEl.matches(".start-button")) {
+  createQuizQuestion(questionStatus); 
+  } 
+};
+var checkAnswerChoice = function(targetEl) {
   var answerId = targetEl.getAttribute("data-id");
   console.log(answerId);
   //clear question
-  var questionClear = document.querySelector(".quiz-question[id='" + questionStatus + "']");
-    if (quizArray[questionStatus].answerKey[answerId]===true) {
+  //var questionClear = document.querySelector(".quiz-question[id='" + questionStatus + "']");
+  var questionClear = document.querySelector(".quiz-question");
+  questionClear.remove();
+  if (quizArray[questionStatus].answerKey[answerId]===true) {
    //create the answer message
     var answerResultEl = document.createElement("div");
     answerResultEl.setAttribute ("class", "quiz-question");
-    answerResultEl.setAttribute ("id", questionStatus+1);
+    answerResultEl.setAttribute ("id", questionStatus);
     answerResultEl.textContent = "Correct!";
+    console.log(answerResultEl);
     pageContentEl.appendChild(answerResultEl);
     quizScore++;
   } else {
     var answerResultEl = document.createElement("div");
     answerResultEl.setAttribute ("class", "quiz-question");
-    answerResultEl.setAttribute ("id", questionStatus+1);
+    answerResultEl.setAttribute ("id", questionStatus);
     answerResultEl.textContent = "Wrong!";
     pageContentEl.appendChild(answerResultEl);
   }
-  questionClear.remove();
   questionStatus++;
+  if (questionStatus < quizArray.length && timeRemaining>0) {
   createQuizQuestion(questionStatus);}
-  else if (targetEl.matches(".start-button")) {
-  createQuizQuestion(questionStatus); 
+  else {console.log("end of the quiz!");
+  createFinalScore();
   }
+}
 
-};
-var checkAnswerChoice = function(event) {
   //compare the answer choice to answer key
   //if answer key is true
     //add answer to score
   //if answer key is false
     //subtract time
     //move on to next question
-}
+
 
 
   //When the correct button is pressed, the user is told it is correct and the quiz moves on
